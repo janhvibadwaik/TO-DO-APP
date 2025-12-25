cda8cd4b0715bf45e34a77826b738d9993b27835
janhvibadwaik <badwaikjanhvi@gmail.com>
2025-12-25 10:22:31 +0530

    some list of items added  by default

---
+++ app.js
@@ -0,0 +1,7 @@
+const todoItems = [
+            "kapde Dhona hai",
+            "gym jana hai",
+            "code karna hai",
+            "market jana hai",
+            "dost se milna hai"
+        ];
